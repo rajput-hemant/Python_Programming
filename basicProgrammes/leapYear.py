d31adbd7134b49990e3c6f60d5b3a5038a8e4c32
Hemant Rajput <68769346+rajput-hemant@users.noreply.github.com>
2021-03-18 09:50:45 +0530

    Delete leapYear.py

--- basicProgrammes/leapYear.py
+++
@@ -1,6 +0,0 @@
-# Python program to check if year is a Leap Year
-year = int(input('Enter the year='))
-if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
-    print("It's a Leap Year")
-else:
-    print("It's not A Leap Year")
