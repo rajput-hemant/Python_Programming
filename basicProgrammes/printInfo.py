bf9d6d7a969bff3e9bd02283790989dc71f463eb
rajput-hemant <rajput.hemant2001@gmail.com>
2021-03-23 09:48:24 +0530

    Initial Commit

--- basicProgrammes/printInfo.py
+++ basicProgrammes/printInfo.py
@@ -1,1 +1,1 @@
-print("Name\t: Hemant Rajput\nSection : B1\nRoll No : 21\nAddress : Amar Colony opp. Triveni Complex, Goverdhan Road, Mathura")
+print("Name\t: Hemant Rajput\nSection : B1\nRoll No : 21\nAddress : XYZ")
