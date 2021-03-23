a84bfc28c2cfbf0cf7f54975787abffe2b74eceb
rajput-hemant <rajput.hemant2001@gmail.com>
2021-03-23 21:10:51 +0530

    Initial Commit

--- Pattern/HollowStarPyramidPattern.py
+++ Pattern/HollowStarPyramidPattern.py
@@ -1,6 +1,6 @@
 # Python program to print Pyramid Using Star Pattern
 n = int(input('Enter number of rows='))
-i = None
+i = 0
 if n == 1:
     print('*')
 elif n == 2:
@@ -8,9 +8,7 @@ elif n == 2:
     print('***')
 if n > 2:
     n -= 2
-    for i in range(1):
-        print(' ' * (n-i) + '*')
+    print(' ' * (n-i) + '*')
     for i in range(1, n+1):
         print(' ' * (n-i) + '*' + ' ' * i + ' ' * (i-1) + '*')
-    for i in range(1):
-        print('*' * (2*n) + '*')
+    print('*' * (2*n) + '*')
