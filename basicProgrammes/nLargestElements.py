2fb621a115ab0ba38b7fe7f1b9da136587500737
rajput-hemant <rajput.hemant2001@gmail.com>
2021-03-24 14:30:09 +0530

    Initial Commit

--- basicProgrammes/nLargestElements.py
+++ basicProgrammes/nLargestElements.py
@@ -6,7 +6,6 @@ for i in range(n):
     list1.append(input())
 print(f'Your list is={list1}')
 tmp = int(input('How many largest elements you want to find?='))
-tmp_list = list1
 for i in range(tmp):
     lar_ele = max(list1)
     list2.append(lar_ele)
