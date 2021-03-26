af1f097fb24e8e78f88eac50bf6fe44432500ee6
rajput-hemant <rajput.hemant2001@gmail.com>
2021-03-26 09:30:45 +0530

    Initial Commit

--- String/symmetricalOrPallindrome.py
+++ String/symmetricalOrPallindrome.py
@@ -1,1 +1,6 @@
 # Python program to check whether the string is Symmetrical or Palindrome
+st1 = input('Enter the first string=')
+if st1[::] == st1[::-1]:
+    print(f'The Strings {st1} is palindrome')
+elif st1[::] == st1[::]:
+    print(f'The String {st1} is Symmetrical')
