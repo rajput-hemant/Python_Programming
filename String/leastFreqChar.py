78a9711f994cb9cad5a9fe57fa61aa09a6878529
rajput-hemant <rajput.hemant2001@gmail.com>
2021-05-09 19:40:34 +0530

    Initial Commit

--- String/leastFreqChar.py
+++ String/leastFreqChar.py
@@ -6,7 +6,7 @@ for i in str1:
         char_freq[i] += 1
     else:
         char_freq[i] = 1
-# For Single Characters
+# For Single Least Frequent Character
 # print(f"Least frequent character in '{str1}' is= {min(char_freq, key=char_freq.get)}")
 # For All characters with same frequency
 tmp = min(char_freq.values())
